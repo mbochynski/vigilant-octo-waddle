ac2dae3fa33c9179ed245fe20a7fe16c0a362031
MBochynski <matbochynski@gmail.com>
2018-06-20 20:49:03 +0200

    Add route to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { MetersListComponent } from './meters-list/meters-list.component';
 import { MeterDetailsComponent } from './meter-details/meter-details.component';
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: 'meters', component: MetersListComponent },
   { path: 'meter/:id', component: MeterDetailsComponent },
   { path: '', redirectTo: '/meters', pathMatch: 'full' },
