d413d03c6c2d78a11daca6d1e4add92899b25f6f
MBochynski <matbochynski@gmail.com>
2018-06-22 23:47:47 +0200

    Fix Path to AuthService

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../auth/auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
