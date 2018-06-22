e3a9fa298d551a37594d8a3333756b45d1ca3816
MBochynski <matbochynski@gmail.com>
2018-06-22 23:46:19 +0200

    Add tests checking setting states onAuthStateChanged

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthService } from './auth.service';
+import * as firebase from 'firebase';
 
 describe('AuthService', () => {
   beforeEach(() => {
@@ -9,9 +10,30 @@ describe('AuthService', () => {
       imports: [RouterTestingModule],
       providers: [AuthService]
     });
+
+
+    const auth = firebase.auth();
+    spyOn(auth, 'onAuthStateChanged').and.callFake(function(cb) {
+      cb({
+        isAnonymous: true,
+        uid: '1234',
+      });
+    });
   });
 
   it('should be created', inject([AuthService], (service: AuthService) => {
     expect(service).toBeTruthy();
+  }));
+  
+  it('should set user to be authorized onAuthStateChanged', inject([AuthService], (service: AuthService) => {
+    expect(service.isAuthorized).toBeTruthy();
+  }));
+  
+  it('should set uid on onAuthStateChanged', inject([AuthService], (service: AuthService) => {
+    expect(service.uid).toBe('1234');
+  }));
+  
+  it('should set anonymous state to true', inject([AuthService], (service: AuthService) => {
+    expect(service.isAnonymous).toBeTruthy();
   }));
 });
