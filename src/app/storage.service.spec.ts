1a512773456a37814af51d9727989f5fea677b5b
MBochynski <matbochynski@gmail.com>
2018-06-24 12:41:56 +0200

    Implement listening on user changes in storage service

--- src/app/storage.service.spec.ts
+++ src/app/storage.service.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing'
 
 import { StorageService } from './storage.service';
+import { AuthService } from './auth/auth.service';
 
 describe('StorageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [StorageService]
+      imports: [RouterTestingModule],
+      providers: [StorageService, AuthService]
     });
   });
 
--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { firestore } from 'firebase';
+import { AuthService } from './auth/auth.service';
+import { Subscription } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class StorageService {
+@Injectable()
+export class StorageService implements OnDestroy {
+  private userSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { 
+    console.log('create user subscription in storage service');
+    
+    this.userSubscription = this.authService.userSubject.subscribe(
+      (user) => {
+        if (user) {
+          this.startFetchingData(user.uid);
+        } else {
+          this.stopFetchingData();
+        }
+      },
+      this.stopFetchingData.bind(this),
+      this.stopFetchingData.bind(this)
+    );
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+  }
+
+  startFetchingData(uid: string) {
+    console.log('start fetching data', uid);
+    
+  }
+
+  stopFetchingData() {
+    console.log('stop fetching data');
+
+  }
 }
